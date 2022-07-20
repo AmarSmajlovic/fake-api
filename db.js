0e1e9cbad169b52db8007b04254037948b15e0b2
AmarSmajlovic <amarcsgo@gmail.com>
2022-07-20 14:52:02 +0200

    separete tables for database

---
+++ db.js
@@ -0,0 +1,13 @@
+const users = require('./data/users');
+const questions = require('./data/questions');
+const answers = require('./data/answers');
+const likes = require('./data/likes');
+const dislikes = require('./data/dislikes');
+
+module.exports = {
+  users,
+  questions,
+  answers,
+  likes,
+  dislikes
+}
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const data = require('./db.js');
+
 const jsonServer = require('json-server');
 const auth = require('json-server-auth')
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
